067921686ae37eefaa4e278db0cdb6c1be65bae8
giuliabferreira <giuliabferreira@hotmail.com>
2023-03-02 15:12:43 -0300

    .

--- src/main/java/br/com/etec/giulia/cursoapi/model/Aluno.java
+++ src/main/java/br/com/etec/giulia/cursoapi/model/Aluno.java
@@ -1,4 +1,45 @@
 package br.com.etec.giulia.cursoapi.model;
 
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "aluno")
 public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String nomealuno;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomealuno() {
+        return nomealuno;
+    }
+
+    public void setNomealuno(String nomealuno) {
+        this.nomealuno = nomealuno;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aluno aluno = (Aluno) o;
+        return id.equals(aluno.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
--- src/main/java/br/com/etec/giulia/cursoapi/model/Cidade.java
+++ src/main/java/br/com/etec/giulia/cursoapi/model/Cidade.java
@@ -1,4 +1,52 @@
 package br.com.etec.giulia.cursoapi.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = "cidade")
 public class Cidade {
+
+    private Integer id;
+    private String nomecidade;
+    private String uf;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNomecidade() {
+        return nomecidade;
+    }
+
+    public void setNomecidade(String nomecidade) {
+        this.nomecidade = nomecidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cidade cidade = (Cidade) o;
+        return id.equals(cidade.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
 }
